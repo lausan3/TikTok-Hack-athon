21ad396f7887a3bea04227c1dce19c1d95272618
lausan3 <lauanthony4455@gmail.com>
2024-08-16 23:41:51 -0400

    add: test files from night before

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main Website",
+		})
+	})
+
+	router.Run()
+}
