cee1aec38a263af04674503837041242d6d6d735
lausan3 <lauanthony4455@gmail.com>
2024-08-17 15:34:51 -0400

    🔧 add config: tailwind

---
+++ site/scripts/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  content: ['./site/**/*.{html,js}'],
+  theme: {
+    colors: {
+      "brand-primary": '#fc3d3d',
+      "brand-secondary": '#3d83fc',
+      "bg-primary": '#F5F5F5',
+      "bg-secondary": '#E5E5E5',
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+  },
+}
