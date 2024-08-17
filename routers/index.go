c79931619f50403948294fc210d737a8aa220b81
lausan3 <lauanthony4455@gmail.com>
2024-08-17 13:36:39 -0400

    ♻️ refactor: database routes to its own route file

--- routers/index.go
+++ routers/index.go
@@ -2,7 +2,6 @@ package routers
 
 import (
 	"database/sql"
-	"main/db/migrations"
 	routers "main/routers/routes"
 
 	"net/http"
@@ -11,27 +10,14 @@ import (
 )
 
 func RegisterRoutes(router *gin.Engine, db *sql.DB) {
-	router.LoadHTMLGlob("site/templates/*")
+	router.LoadHTMLGlob("site/templates/*.html")
 
 	// 404 Route
 	router.NoRoute(func(ctx *gin.Context) {
 		ctx.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "Route Not Found"})
 	})
 	router.GET("/health", func(ctx *gin.Context) { ctx.JSON(http.StatusOK, gin.H{"live": "ok"}) })
-	router.GET("/db/migrate/users", func(c *gin.Context) {
-		migrations.MigrateUsersTable(db)
-
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Users table migration was successful",
-		})
-	})
-	router.GET("/db/migrate/posts", func(c *gin.Context) {
-		migrations.MigratePostsTable(db)
-
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Posts table migration was successful",
-		})
-	})
 
 	routers.IndexRoutes(router)
+	routers.DatabaseRoutes(router, db)
 }
