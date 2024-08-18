651b935d1e2e568f99c07566f35e86c7178a0150
Marouane <57333995+riimuru@users.noreply.github.com>
2024-08-17 22:07:26 -0400

    refactor(models): Remove commented out code in post model

--- server/models/post.go
+++ server/models/post.go
@@ -7,17 +7,6 @@ import (
 	"main/infra/utils"
 )
 
-/**
-CREATE TABLE IF NOT EXISTS posts (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	user_id INT,
-	title VARCHAR(255) NOT NULL,
-	content TEXT NOT NULL,
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY (user_id) REFERENCES users(id)
-);
-*/
-
 type Post struct {
 	ID        int    `json:"id"`
 	UserID    int    `json:"user_id"`
