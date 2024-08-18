ce0099173220a1fb424b31f16a3b6b72bf1ff566
Marouane <57333995+riimuru@users.noreply.github.com>
2024-08-17 22:04:05 -0400

    refactor(routes): Use route grouping in users

--- server/routers/routes/api/migrate.go
+++ server/routers/routes/api/migrate.go
@@ -9,16 +9,16 @@ import (
 )
 
 func MigrationRoutes(router *gin.RouterGroup, db *sql.DB) {
-	database := router.Group("/migrate")
+	group := router.Group("/migrate")
 
-	database.GET("users", func(c *gin.Context) {
+	group.GET("users", func(c *gin.Context) {
 		migrations.MigrateUsersTable(db)
 
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Users table migration was successful",
 		})
 	})
-	database.GET("posts", func(c *gin.Context) {
+	group.GET("posts", func(c *gin.Context) {
 		migrations.MigratePostsTable(db)
 
 		c.JSON(http.StatusOK, gin.H{
