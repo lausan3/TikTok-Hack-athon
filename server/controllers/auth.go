3505e8da7d69974c0aa2ff25fa176ab2f96cb880
Marouane <57333995+riimuru@users.noreply.github.com>
2024-08-17 22:28:30 -0400

    refactor(auth): Separate user registration logic into AuthController

--- server/controllers/auth.go
+++ server/controllers/auth.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"main/forms"
 	validator "main/infra/utils/validator"
+	"main/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -34,4 +35,30 @@ func (u *AuthController) LoginUser(c *gin.Context, db *sql.DB) (token validator.
 	c.JSON(http.StatusOK, token)
 
 	return token, nil
+}
+
+func (u *AuthController) RegisterUser(c *gin.Context, db *sql.DB) (user models.User, err error) {
+	// get the request body and bind it to the RegisterForm struct
+	var registerForm forms.RegisterForm
+
+	if err := c.ShouldBindJSON(&registerForm); err != nil {
+		message := userForm.Register(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": message,
+		})
+		return user, err
+	}
+
+	if err := userModel.Register(registerForm, db); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return user, err
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "User created successfully",
+	})
+
+	return user, nil
 }
